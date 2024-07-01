da6a9a77c6a910074cc9ff658d7f05d8a58ab1e9
michelllepan <michellepan8@gmail.com>
2024-07-01 15:58:13 -0700

    Enable advanced mode

--- src/camera.py
+++ src/camera.py
@@ -4,15 +4,21 @@ import pyrealsense2 as rs
 class RealSenseCamera:
 
     def __init__(self, width=1280, height=720):
+        self.pipeline = rs.pipeline()
         config = rs.config()
+
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = config.resolve(pipeline_wrapper)
+        device = pipeline_profile.get_device()
+        advanced_mode = rs.rs400_advanced_mode(device)
+        advanced_mode.toggle_advanced_mode(True)
+
         config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
         config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
+        self.pipeline.start(config)
 
         self.frame_history = []
-
         self._setup_postprocessing()
-        self.pipeline = rs.pipeline()
-        self.pipeline.start(config)
 
     def _setup_postprocessing(self):
         self.align = rs.align(rs.stream.color)
@@ -37,7 +43,7 @@ class RealSenseCamera:
             return
         
         self.frame_history.append(depth_frame)
-        if len(self.frame_history) > 10:
+        if len(self.frame_history) > 15:
             self.frame_history.pop(0)
         
         # apply filters according to order in
